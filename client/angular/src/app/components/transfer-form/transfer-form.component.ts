941c1ada0598dc18d44fcf5526e815755753bda3
Demeter Bálint <demibalint@gmail.com>
2025-09-09 13:32:10 +0200

    transfer-form loading properly

--- client/angular/src/app/components/transfer-form/transfer-form.component.ts
+++ client/angular/src/app/components/transfer-form/transfer-form.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AccountService} from '../../services/account.service';
+import {AccountDetails} from '../../models/account-details';
 
 @Component({
   selector: 'app-transfer-form',
@@ -6,6 +9,48 @@ import { Component } from '@angular/core';
   templateUrl: './transfer-form.component.html',
   styleUrl: './transfer-form.component.css'
 })
-export class TransferFormComponent {
+export class TransferFormComponent implements OnInit {
 
+  transferForm: FormGroup;
+  targetList: AccountDetails[] = [];
+  myAccount!: AccountDetails;
+
+  constructor(private fb: FormBuilder, private accountService: AccountService) {
+    this.transferForm = fb.group({
+      source: [''],
+      target: ['', Validators.required],
+      amount: ['', Validators.required],
+      myBalance: ['']
+    })
+  }
+
+  ngOnInit(): void {
+    this.loadTransferForm();
+
+    this.transferForm.get('source')!.disable();
+    this.transferForm.get('myBalance')!.disable();
+  }
+
+  private loadTransferForm() {
+    this.accountService.getAccountDetails().subscribe(
+      (data: AccountDetails) => {
+        this.myAccount = data;
+
+        this.accountService.getAllAccounts().subscribe(
+          (data: AccountDetails[])=> {
+            this.targetList = data.filter(ac => ac.id !== this.myAccount.id);
+
+            this.transferForm.patchValue({
+              source: this.myAccount.username,
+              myBalance: this.myAccount.balance
+            })
+          },
+          error => {}
+        )
+      },
+      error => {}
+    )
+  }
+
+  protected readonly require = require;
 }
